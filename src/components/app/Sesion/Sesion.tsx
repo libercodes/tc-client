5475840c1c8ca864c213079302a22a264b3261ea
liber <libermenna@gmail.com>
2020-05-16 00:13:11 -0300

    Added searchbar

--- src/components/app/Sesion/Sesion.tsx
+++ src/components/app/Sesion/Sesion.tsx
@@ -6,16 +6,24 @@ import {
     Container, 
     Col, 
     Row,
-    Button
+    Button,
+    Form
 } from 'react-bootstrap'
 import { UserContext } from '../../../context/context'
 import { State, Usuario } from '../../../utils/types'
 import moment from 'moment'
-import { PersonAdd } from '@material-ui/icons'
+import { Search } from '@material-ui/icons'
 
 const SesionComponent = () => {
     const state : State = useContext(UserContext).state
     const dispatch = useContext(UserContext).dispatch
+    const [ filteredList, setFilteredList ] = useState(state.listaDeSesiones)
+    useEffect(() => setFilteredList(state.listaDeSesiones), [state.listaDeSesiones])
+
+    const FilterList = (query: string) => 
+        setFilteredList(state.listaDeSesiones.filter(sesion => sesion.usuario.nombreDeUsuario.toUpperCase().includes(query.toUpperCase())))
+
+        
     useEffect(() => {
         const getSesiones = async() => {
             let { data } = await axios.get('/api/admin/sesiones', axiosConfig(state.credentials.token))
@@ -37,13 +45,25 @@ const SesionComponent = () => {
                 <Row className="justify-content-center">
                     <Col sm={8} className="bg-primary text-light py-2">
                         <Row>
-                            <Col sm={6} className="text-center">
-                                <h2>Administrar <b>usuarios</b></h2>
+                            <Col sm={12} className="text-center">
+                                <h2>Consultar <b>sesiones</b></h2>
                             </Col>
-                            <Col sm={6} className="text-center">
-                                <Button variant="success"> 
-                                    <PersonAdd/> Agregar usuario
-                                </Button>
+                            
+                        </Row>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center my-3">
+                    <Col sm={8} className="bg-light py-2 rounded">
+                        <Row className="justify-content-center">
+                            <Col sm={1} xs={1} className="d-flex align-items-center justify-content-end p-0">
+                                <Search/>
+                            </Col>
+                            <Col sm={6} xs={10}>
+                                <Form.Control 
+                                    type="text"
+                                    placeholder="Nombre de usuario"
+                                    onChange={(e:any) => FilterList(e.target.value)}
+                                    />
                             </Col>
                         </Row>
                     </Col>
@@ -63,7 +83,7 @@ const SesionComponent = () => {
                                 {
                                     (state.listaDeSesiones.length > 0 ) && 
                                     
-                                    state.listaDeSesiones.map(sesion => (
+                                    filteredList.map(sesion => (
                                         <tr key={sesion._id}>
                                             <td>{sesion.usuario.nombreDeUsuario}</td>
                                             <td>{moment(sesion.fechaDeInicio).format('LLL')}</td>
@@ -73,7 +93,6 @@ const SesionComponent = () => {
                                 }
                             </tbody>
                         </Table>
-
                     </Col>
                 </Row>
             </Container>
